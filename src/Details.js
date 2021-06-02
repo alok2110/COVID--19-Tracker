634dbe6a83bfd47d23a2b7df0ffa71b27194892b
alok2110 <aloksaxena755@gmail.com>
2021-06-02 10:18:39 +0530

    initail commit

---
+++ src/Details.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import Summary from "./Summary";
+import Countries from "./Countries";
+
+class Details extends Component {
+
+    state = {
+        countries: [],
+        global: [],
+        currentDate: null,
+        loading: true
+    }
+
+    async componentDidMount() {
+       const res = await axios.get('https://api.covid19api.com/summary');
+       this.setState({ countries: res.data.Countries });
+       this.setState({ global: res.data.Global });
+       this.setState({ currentDate: res.data.Date });
+       this.setState({ loading: false });
+
+    }
+    render() {
+        if(this.state.loading) {
+           return <h1>Loading...</h1>
+        }
+        return (
+            <>
+                <Summary summary={this.state.global} currentDate={this.state.currentDate} />
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Country</th>
+                            <th>New Confirmed</th>
+                            <th>Total Recovered</th>
+                            <th>Total Death</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.countries.map(country => (
+                            <Countries countries={country} key={country.Country} />
+                        ))}
+                    </tbody>
+                </table>
+            </>
+        )
+    }
+}
+
+export default Details
